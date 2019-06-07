c673b106c76faecfadce05148893bbf1822b30d3
DannisLi <zimengli@buaa.edu.cn>
2019-06-07 17:05:33 +0800

    112

--- code/test.py
+++ code/test.py
@@ -7,20 +7,29 @@ os.system('nvcc main.cu -w')
 
 name = 'kos'
 
-perp_list, time_list = [],[]
+n_epoch_list = list(range(10, 201, 10))
+cpu_perp_list, cpu_time_list = [],[]
+gpu_perp_list, gpu_time_list = [],[]
 
-for n_epoch in range(10, 201, 10):
-    # res = os.popen('./a.out kos 4 1 %d 256 5' % n_epoch)
-    res = os.popen('./a.out kos 4 0 %d' % n_epoch)
-    res = res.readlines()
-    # print(res)
-    perp = float(res[0].strip())
-    time = int(res[1].strip())
-    print(n_epoch, perp, time)
-    perp_list.append(perp)
-    time_list.append(time)
+for n_epoch in n_epoch_list:
+    gpu_res = os.popen('./a.out kos 4 1 %d 256 5' % n_epoch)
+    cpu_res = os.popen('./a.out kos 4 0 %d' % n_epoch)
+    
+    gpu_res = gpu_res.readlines()
+    cpu_res = cpu_res.readlines()
+    
+    gpu_perp = float(gpu_res[0].strip())
+    cpu_perp = float(cpu_res[0].strip())
+    
+    # time = int(res[1].strip())
+    print(n_epoch, gpu_perp, cpu_perp)
+    cpu_perp_list.append(cpu_perp)
+    gpu_perp_list.append(gpu_perp)
+    # time_list.append(time)
 
 
-plt.plot(list(range(10,201,10)), perp_list)
+plt.plot(n_epoch_list, cpu_perp_list, label='cpu', c='b')
+plt.plot(n_epoch_list, gpu_perp_list, label='gpu', c='r')
+plt.legend()
 plt.savefig('perp.png')
 
