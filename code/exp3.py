dc32a7ddfb06935995999fee615ec03d9baed59a
DannisLi <zimengli@buaa.edu.cn>
2019-06-07 20:29:13 +0800

    dslfksd;f

--- code/exp3.py
+++ code/exp3.py
@@ -16,10 +16,10 @@ gpu_perp_list, gpu_time_list = [],[]
 for thread_num in thread_num_list:
     gpu_perp,gpu_time = 0,0
     for i in range(3):
-        gpu_res = os.popen('./a.out kos 4 1 60 %d 5 2' % thread_num)
+        gpu_res = os.popen('./a.out kos 4 1 60 %d 5 0' % thread_num)
         gpu_res = gpu_res.readlines()
-        gpu_perp += float(gpu_res[0].strip()) / 5.
-        gpu_time += int(gpu_res[1].strip()) / 5.
+        gpu_perp += float(gpu_res[0].strip()) / 3.
+        gpu_time += int(gpu_res[1].strip()) / 3.
     
     print(thread_num, gpu_perp, gpu_time)
     gpu_perp_list.append(gpu_perp)
---
+++ code/exp5.py
@@ -0,0 +1,41 @@
+# coding:utf8
+
+'''
+sync_epoch kos
+'''
+
+import os
+import matplotlib.pyplot as plt
+
+# os.system('nvcc main.cu -w')
+
+
+sync_epoch_list = [2, 4, 6, 10, 15]
+gpu_perp_list, gpu_time_list = [],[]
+
+for sync_epoch in sync_epoch_list:
+    gpu_perp,gpu_time = 0,0
+    for i in range(5):
+        gpu_res = os.popen('./a.out kos 4 1 60 256 %d 2' % sync_epoch)
+        gpu_res = gpu_res.readlines()
+        gpu_perp += float(gpu_res[0].strip()) / 5.
+        gpu_time += int(gpu_res[1].strip()) / 5.
+    
+    print(sync_epoch, gpu_perp, gpu_time)
+    gpu_perp_list.append(gpu_perp)
+    gpu_time_list.append(gpu_time)
+    
+
+fig = plt.figure(figsize=(24,12))
+ax = fig.add_subplot(111)
+ax.plot(sync_epoch_list, gpu_perp_list, color='b', marker='o', label='log likelihood')
+ax2 = ax.twinx()
+ax2.plot(sync_epoch_list, gpu_time_list, color='r', marker='o', label='used time')
+
+fig.legend()
+
+ax.set_xlabel('epochs per sync')
+ax.set_ylabel('log likelihood')
+ax2.set_ylabel('used time (s)')
+plt.savefig('kos_sync_epoch.png')
+plt.close()
